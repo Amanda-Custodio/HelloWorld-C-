a420730bbe7949551cf4bbcbc59d3cb2ced7f5d0
Amanda Custódio <amandaccustodio@hotmail.com>
2022-07-12 17:14:26 -0300

    Subindo as atividades

---
+++ Atividade-1-helloworld/Atividade-1-helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade_1_helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
